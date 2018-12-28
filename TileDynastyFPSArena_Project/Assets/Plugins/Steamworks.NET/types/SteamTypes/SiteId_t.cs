c064457112d7f326cab62713ed4255d4a620e919
Peter Tan <purpl3grape@gmail.com>
2018-12-28 11:37:53 -0500

    Project Updated

--- TileDynastyFPSArena_Project/Assets/Plugins/Steamworks.NET/types/SteamTypes/SiteId_t.cs
+++ TileDynastyFPSArena_Project/Assets/Plugins/Steamworks.NET/types/SteamTypes/SiteId_t.cs
@@ -16,47 +16,48 @@ using IntPtr = System.IntPtr;
 
 namespace Steamworks {
 	[System.Serializable]
-	public struct HSteamPipe : System.IEquatable<HSteamPipe>, System.IComparable<HSteamPipe> {
-		public int m_HSteamPipe;
+	public struct SiteId_t : System.IEquatable<SiteId_t>, System.IComparable<SiteId_t> {
+		public static readonly SiteId_t Invalid = new SiteId_t(0);
+		public ulong m_SiteId;
 
-		public HSteamPipe(int value) {
-			m_HSteamPipe = value;
+		public SiteId_t(ulong value) {
+			m_SiteId = value;
 		}
 
 		public override string ToString() {
-			return m_HSteamPipe.ToString();
+			return m_SiteId.ToString();
 		}
 
 		public override bool Equals(object other) {
-			return other is HSteamPipe && this == (HSteamPipe)other;
+			return other is SiteId_t && this == (SiteId_t)other;
 		}
 
 		public override int GetHashCode() {
-			return m_HSteamPipe.GetHashCode();
+			return m_SiteId.GetHashCode();
 		}
 
-		public static bool operator ==(HSteamPipe x, HSteamPipe y) {
-			return x.m_HSteamPipe == y.m_HSteamPipe;
+		public static bool operator ==(SiteId_t x, SiteId_t y) {
+			return x.m_SiteId == y.m_SiteId;
 		}
 
-		public static bool operator !=(HSteamPipe x, HSteamPipe y) {
+		public static bool operator !=(SiteId_t x, SiteId_t y) {
 			return !(x == y);
 		}
 
-		public static explicit operator HSteamPipe(int value) {
-			return new HSteamPipe(value);
+		public static explicit operator SiteId_t(ulong value) {
+			return new SiteId_t(value);
 		}
 
-		public static explicit operator int(HSteamPipe that) {
-			return that.m_HSteamPipe;
+		public static explicit operator ulong(SiteId_t that) {
+			return that.m_SiteId;
 		}
 
-		public bool Equals(HSteamPipe other) {
-			return m_HSteamPipe == other.m_HSteamPipe;
+		public bool Equals(SiteId_t other) {
+			return m_SiteId == other.m_SiteId;
 		}
 
-		public int CompareTo(HSteamPipe other) {
-			return m_HSteamPipe.CompareTo(other.m_HSteamPipe);
+		public int CompareTo(SiteId_t other) {
+			return m_SiteId.CompareTo(other.m_SiteId);
 		}
 	}
 }
